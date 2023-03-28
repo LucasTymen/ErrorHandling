0391621458fe93849357a2257483675e3826879a
Lucas Tymen <lucas.tymen@gmail.com>
2023-03-28 15:10:47 +0200

    setp 5

--- error_5_theTryCatchStatement.js
+++ error_5_theTryCatchStatement.js
@@ -77,5 +77,28 @@ try {
 Concept Review
 Want to quickly review some of the concepts you’ve been learning? Take a look at this material's cheatsheet!
 ========================================================================================================================
+code throwing the error
+*/
+try {
+  // try block code
+  throw new Error('This was a mistake');
+} catch (e){
+//catch block code;
+console.log(e)
+};
 
+/*
+========================================================================================================================
+displayed Error :
+Error: This was a mistake
+    at Object.<anonymous> (/home/ccuser/workspace/error-handling-try-catch-i/main.js:5:9)
+    at Module._compile (module.js:571:32)
+    at Object.Module._extensions..js (module.js:580:10)
+    at Module.load (module.js:488:32)
+    at tryModuleLoad (module.js:447:12)
+    at Function.Module._load (module.js:439:3)
+    at Module.runMain (module.js:605:10)
+    at run (bootstrap_node.js:427:7)
+    at startup (bootstrap_node.js:151:9)
+    at bootstrap_node.js:542:3
 */
